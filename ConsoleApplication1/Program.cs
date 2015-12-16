f089675e666f83ba00c2830694adeaeb45274a72
Julia <juliakisa@ukr.net>
2015-12-16 11:29:10 +0200

    New files :D

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Math {
+
+        int A;
+        int B;
+        int C;
+        string G;
+        Math(string name) : this(200,30) {
+            this.G = name;
+        }
+
+        Math(int A, int B){
+            this.A = A;
+            this.B = B;
+    }
+        Math(int A, int B, int C)
+        {
+
+        }
+        
+    }
+
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Math A = new Math();
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
